22d0cf6000f347ef9c9353f947167acc67cbe6c9
haleighdalke <haleighdalke@gmail.com>
2021-04-30 17:53:14 -0400

    basic useReducer functionality

---
+++ src/components/Calculator.js
@@ -0,0 +1,38 @@
+import React, {useReducer} from 'react'
+
+function Calculator() {
+    // const initialState = {
+    //     count: 0
+    // }
+
+    const reducer = (state, action) => {
+        switch (action) {
+            case 'add':
+                return state + 1
+            case 'subtract':
+                return state - 1
+            case 'divide':
+                return state / 2
+            case 'multiply':
+                return state * 2
+            default:
+                return state
+        }
+    }
+
+    const [count, dispach] = useReducer(reducer, 0)
+
+    return (
+        <div>
+            <button onClick={() => dispach('add')}>Add</button>
+            <button onClick={() => dispach('subtract')}>subtract</button>
+            <button onClick={() => dispach('divide')}>divide</button>
+            <button onClick={() => dispach('multiply')}>multiply</button>
+            <div>
+                Count: {count}
+            </div>
+        </div>
+    )
+}
+
+export default Calculator
