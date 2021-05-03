21108360540dd9c539a7cedda21a1776313ff695
haleighdalke <haleighdalke@gmail.com>
2021-05-03 15:38:04 -0400

    refactored logic with regex

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -5,7 +5,7 @@ function Calculator() {
 
     const [expression, setExpression] = useState("")
     const [evaluation, setEvaluation] = useState(0)
-    const action = ""
+    const [action, setAction] = useState("")
 
     let evaluate = (arr, operand) => {
         switch (operand) {
@@ -23,6 +23,45 @@ function Calculator() {
     }
 
     useEffect(() => {
+        // if expression matches 24x (slice operator and store globally)
+        // if expression matches 24x38 (do nothing)
+        // if expression matches 24x38= (evaluate and return product)
+        // if expression matches 24x38+ (slice operator and store globally, evaluate expression, concatinate back together, display)
+        // if expression matches 38++ (slice operator and replace, concatinate back together)
+        // if expression matches 38+= regexp: [0-9]|\+|-|x|\/)*=$
+
+
+        // PROBLEM: ALL GETTING CAUGHT HERE IN FIRST IF STATEMENT, MUST MODIFY MATCH TO BE A FULL MATCH
+        if(expression.match("([0-9]+(x|\\+|-|\\/){1}){1}")){
+            console.log("caught in 1 if")
+            setAction(expression.slice(-1))
+        }else if(expression.match("[0-9]+(x|\\+|-|\\/){1}[0-9]+")){
+            // do nothing
+            console.log("caught in 2 if")
+        }else if(expression.match("[0-9]+(x|\\+|-|\\/){1}")){
+            console.log("caught in 3 if")
+            setEvaluation(evaluate(expression.split("(x|\\+|-|\\/)"), action))
+            if(evaluation == "error"){
+                setExpression("")
+                setAction("")
+            }else{
+                setAction(expression.slice(-1))
+                setExpression(evaluation + action)
+            }
+        }else if(expression.match("[0-9]+(x|\\+|-|\\/){2}")){
+            console.log("caught in 4 if")
+            setAction(expression.slice(-1))
+            setExpression(expression.slice(0, -2) + action)
+        }else if(expression.match("([0-9]|\\+|-|x|\\/){2,}=$")){
+            console.log("caught in 5 if")
+            setEvaluation(evaluate([expression.split("(x|\\+|-|\\/)")[0], expression.split("(x|\\+|-|\\/)")[0]], action))
+        }else {
+            // do nothing
+        }
+
+
+
+
         // evaluate here
         // evaluation = expression.match("[0-9]+(x|\+|-|/){1}") ? action = expression.slice(-1) :
         //     expression.match("[0-9]+(x|\+|-|/){1}[0-9]+") ? setEvaluation(evaluate(expression.split("[0-9]+(x|\+|-|/){1}[0-9]+", action))) : 
@@ -33,6 +72,10 @@ function Calculator() {
 
     return (
         <Container>
+            {console.log("expression: " + expression)}
+            {console.log("evaluation: " + evaluation)}
+            {console.log("action: " + action)}
+
             <h1>Calculator App Using React Hooks</h1>
             <Table className="col-2 offset-5" borderless>
                 <thead>
